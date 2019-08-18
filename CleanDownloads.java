83591861967dab39c6ccf85227bf340b778453d2
STERLING-HOME\Sterling <sterlingmcleod9@gmail.com>
2019-08-18 11:00:07 -0400

    Added: saving which files were unmarked so that they are not displayed again, setting new threshold value, not checking the previously unmarked files (displaying all files again), running without saving the unmarked files, and cmd-line args for these features.

--- CleanDownloads.java
+++ CleanDownloads.java
@@ -1,4 +1,5 @@
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -7,6 +8,7 @@ import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,18 +16,29 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import javax.swing.*;
 import java.lang.System;
+import java.io.BufferedWriter;
+import java.util.Scanner;
 
 
 class CleanDownloads extends JFrame
 {
 
-    private static ArrayList<File> oldFiles;
+    private static final long serialVersionUID = 1L;
+
+    // ArrayLists to hold files
+	private static ArrayList<File> oldFiles;
     private static ArrayList<File> toDelete;
+    private static ArrayList<String> notToDelete;
     private static ArrayList<JCheckBox> checkBoxes;
+    private static int numDaysThreshold = 30;
+    private static boolean oneTime = false;
+    private static boolean checkNotToDelete = true;
+
     
     // debugging
     private static ArrayList<File> notOldFiles;
     
+    // GUI members
     private JPanel panel;
     private JScrollPane scrollPane;
     private JButton deleteButton;
@@ -33,7 +46,6 @@ class CleanDownloads extends JFrame
     
     public CleanDownloads() throws IOException
     {
-        populateFileLists();
         setTitle("Clean Downloads");
         
         // Create the panel
@@ -41,24 +53,116 @@ class CleanDownloads extends JFrame
         panel.setMinimumSize(new Dimension(600,400));
         panel.setBackground(Color.DARK_GRAY);
         panel.setAlignmentX(LEFT_ALIGNMENT);
-        
+        panel.setLayout(new GridBagLayout());
+
+
+        // Add the panel to the frame
         add(panel);
-        
+
+        // Set close operation
+        setCloseOperation();
+
+        // Create all the components
+        createComponents(true);
+    }
+
+    public void createComponents(boolean checkNotDeleting)
+    {
+        createInstructionsLabel();
+        createCheckBoxes();
+        createScrollPane();
+        createButton();
+    }
+
+    public void createInstructionsLabel()
+    {
+        // Label telling the user what to do.
         instructionsLabel = new JLabel("Select which files you would like to delete.");
+
+        // Align the label
+        // Don't know why the text isn't left-aligned. None of the line below have worked.
+        // Maybe something with the panel layout?
         instructionsLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        instructionsLabel.setAlignmentX(LEFT_ALIGNMENT);
+        instructionsLabel.setHorizontalTextPosition(SwingConstants.LEFT);
         instructionsLabel.setForeground(Color.CYAN);
 
+        // Create a grid bag constraints
         GridBagConstraints positionConst = new GridBagConstraints();
         positionConst.gridx = 0;
         positionConst.gridy = 0;
 
+        // Add the instructions label
         panel.add(instructionsLabel, positionConst);
-        // Create gui components
-        createCheckBoxes();
-        createScrollPane();
-        createButton();
-
     }
+
+    // This sets the members numDaysThreshold and notToDelete
+    public static void readNotDeletingFile()
+    {
+        notToDelete = new ArrayList<String>();
+        try
+        {
+            File file = new File("not_deleting.txt");
+            Scanner scanner = new Scanner(file);
+            if(scanner.hasNextInt())
+            {
+                numDaysThreshold = scanner.nextInt();
+            }
+
+            while(scanner.hasNextLine())
+            {
+                notToDelete.add(scanner.nextLine());
+            }
+
+            scanner.close();
+        }
+        catch(Exception e) {System.out.println("Something went wrong with the file not_deleting.txt.");}
+    }
+
+    public void setCloseOperation()
+    {             
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                /*// Confirm closing
+                if (JOptionPane.showConfirmDialog(this, 
+                    "Are you sure you want to close this window?", "Close Window?", 
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+                    System.exit(0);
+                }*/
+
+                try {
+                    // Don't write to file if the 'one-time' option was specified
+                    if(oneTime == false)
+                    {
+                        BufferedWriter writer = new BufferedWriter(new FileWriter("not_deleting.txt"));
+                    
+                        writer.write(Integer.toString(numDaysThreshold)+"\n");
+                        // Save unchecked files
+                        for(int i=0;i<oldFiles.size();i++)
+                        {
+                            // Write the filename
+                            String fileName = oldFiles.get(i).getName();
+    
+                            if(i == oldFiles.size()-1)
+                            {
+                                writer.write(fileName);
+                            }
+                            else
+                            {
+                                writer.write(fileName+"\n");
+                            }                    
+                        }   // end for
+                        writer.close();
+                    }                    
+                }
+            catch(Exception e) {System.out.println("Something went wrong with the file not_deleting.txt.");}
+            }
+        });
+    }
+
+
 
     public void createButton()
     {
@@ -68,7 +172,7 @@ class CleanDownloads extends JFrame
         // Create a position in the gui for the button
         GridBagConstraints positionConst = new GridBagConstraints();
         positionConst.gridx = 0;
-        positionConst.gridy = oldFiles.size();
+        positionConst.gridy = checkBoxes.size()+1;
 
         // Add an action listener to the button
         deleteButton.addActionListener(new ActionListener() { 
@@ -93,18 +197,18 @@ class CleanDownloads extends JFrame
         panel.add(deleteButton, positionConst);
     }
 
-    
+
     public void createCheckBoxes()
     {
         checkBoxes = new ArrayList<>();
         
-        panel.setLayout(new GridBagLayout());
         GridBagConstraints positionConst = new GridBagConstraints();
         
         positionConst.anchor = GridBagConstraints.WEST;
         
         for(int i=0;i<oldFiles.size();i++)
         {
+            // Create a checkbox and add it to the panel
             JCheckBox temp = new JCheckBox(oldFiles.get(i).getName());
             temp.setBackground(Color.DARK_GRAY);
             temp.setForeground(Color.CYAN);
@@ -118,8 +222,7 @@ class CleanDownloads extends JFrame
             panel.add(temp, positionConst);
 
             checkBoxes.add(temp);
-        }
-        
+        }   // end for
     }
     
     public void createScrollPane()
@@ -131,12 +234,26 @@ class CleanDownloads extends JFrame
         scrollPane.setAlignmentX(RIGHT_ALIGNMENT);
         add(scrollPane);
     }
+
     
-    public void populateFileLists() throws IOException
+    public static boolean isNotToDelete(File f)
+    {
+        for(String s : notToDelete)
+        {
+            if(s.equals(f.getName()))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    
+    public static void populateFileLists(boolean checkNotDeleting) throws IOException
     {
         // Get number of days in milliseconds and current time in ms
-        int numDays = 30;
-        long timeThresholdInMs = numDays * 86400000L;
+        long timeThresholdInMs = numDaysThreshold * 86400000L;
         long currentTime = System.currentTimeMillis();
 
         // Get file path
@@ -152,7 +269,6 @@ class CleanDownloads extends JFrame
         {
             downloadsPath += "/Downloads";
         }
-        System.out.println(downloadsPath);
 
         // Get list of files
         File folder = new File(downloadsPath);
@@ -173,13 +289,15 @@ class CleanDownloads extends JFrame
 
             long diff = currentTime - fatr.lastAccessTime().toMillis();
 
-            if(diff > timeThresholdInMs)
+            // Check the time it was last accessed
+            // and check that it hasn't been ignored previously
+            if(diff > timeThresholdInMs && (checkNotDeleting == false || isNotToDelete(file) == false))
             {
-                    oldFiles.add(file);
+                oldFiles.add(file);
             }
             else
             {
-                    notOldFiles.add(file);
+                notOldFiles.add(file);
             }
         }   // end for each
     }
@@ -193,18 +311,91 @@ class CleanDownloads extends JFrame
 
         toDelete.clear();
     }
+
+    public static void displayHelpMessage()
+    {
+        System.out.println("\tUsage: java CleanDownloads [-aho] [-t numberOfDays]");
+        System.out.println("\tCommand line args:");
+        System.out.println("\t\t-a | --all: Display all old files instead of checking not_deleting.txt");
+        System.out.println("\t\t-h | --help: Display this message.");
+        System.out.println("\t\t-o | --one-time: Do not save unmarked files. This option will trigger the -a option.");
+        System.out.println("\t\t-t <value> | --threshold <value>: Sets a new threshold value for marking a file for deletion. This option will trigger the -a option.");
+    }
     
     
+    public static void parseCmdLineAgs(String[] args)
+    {
+        // Show all files with cmd line arg --all or -a
+        if(args.length > 0)
+        {
+            for(int i=0;i<args.length;i++)
+            {
+                // help
+                if(args[0].equals("--h") || args[0].equals("-h"))
+                {
+                    displayHelpMessage();
+                    break;
+                }
+
+                // all
+                if(args[i].equals("--all") || args[i].equals("-a"))
+                {
+                    checkNotToDelete = false;
+                }
+
+                // threshold
+                else if(args[i].equals("--threshold") || args[i].equals("-t"))
+                {
+                    if(args.length < i+2)
+                    {
+                        System.out.println("Missing threshold value. Enter an integer value.");
+                    }
+    
+                    // Set the value
+                    numDaysThreshold = Integer.parseInt(args[i+1]);
+    
+                    // If a threshold is specified, check all files again
+                    checkNotToDelete = false;
+
+                    i++;
+                }
+
+                // one-time
+                else if(args[i].equals("-o") || args[i].equals("--one-time"))
+                {
+                    oneTime = true;
+                }
+            }   // end for      
+            
+            // Do this outside for loop in case -o was set before -t
+            if(oneTime)
+            {
+                checkNotToDelete = false;
+            }
+        }   // end if
+    }
+
+
     public static void main(String[] args) throws Exception
     {
-        System.out.println("Hello world");
+        // Read the contents of not_deleting.txt
+        // and set the ArrayList member notToDelete
+        readNotDeletingFile();
 
-        
-        // Create GUI and start code
-        CleanDownloads myGUI = new CleanDownloads();
-        myGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myGUI.pack();
-        myGUI.setVisible(true);
-        
+        // Do this after reading not_deleting file because
+        // the user may have specified a new threshold
+        parseCmdLineAgs(args);        
+
+        // Populate the ArrayList members that hold files
+        populateFileLists(checkNotToDelete);
+
+        // If there are files to delete, then create GUI and start main loop
+        if(oldFiles.size() > 0)
+        {
+            CleanDownloads myGUI = new CleanDownloads();
+            myGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myGUI.pack();
+            myGUI.setVisible(true);
+        }
     }   // end main
 }
